430acde2a7b3553b80284131fc98eecc8235db14
Atif Khan <wahabshah10200@gmail.com>
2026-01-06 23:23:01 +0500

    Learned graphs ds in python

--- dsa/graphs.py
+++ dsa/graphs.py
@@ -1,34 +1,41 @@
-n = 8
-
+from collections import defaultdict
+n  = 8
 A = [[0,1],[1,2],[0,3],[3,4],[3,6],[3,7],[4,2],[4,5],[5,2]]
 
 M = []
+
 for i in range(n):
     M.append([0]*n)
 
+# convert the input list into adjacency matrix
 for u,v in A:
     M[u][v] = 1
+    M[v][u] = 1
 
-for i in M:
-    print(i)
+for row in M:
+    print(*row)
 
-from collections import defaultdict
+# convert the input list into adjacency list
 
 D = defaultdict(list)
-for i,j in A:
-    D[i].append(j)
-print(D)
+
+for u,v in A:
+    D[u].append(v)
+    # D[v].append(u) # for undirected
+
+# DFS
 
 def dfs_recursive(node):
     print(node)
-    for nei_node in D[node]:
-        if nei_node not in seen:
-            seen.add(nei_node)
-            dfs_recursive(nei_node)
-        # print(nei_node)
+
+    for i in D[node]:
+        if i not in seen:
+            seen.add(i)
+            dfs_recursive(i)
+
+
 source = 0
+
 seen = set()
 seen.add(source)
 dfs_recursive(source)
-
-print(2 not in [1,2])
