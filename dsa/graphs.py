cd2aed335bf892d0603fdd3d0d6f187fa7d05295
Atif Khan <wahabshah10200@gmail.com>
2026-01-11 22:36:07 +0500

    practiced some data cleaning

--- dsa/graphs.py
+++ dsa/graphs.py
@@ -1,20 +1,8 @@
 from collections import defaultdict
-n  = 8
-A = [[0,1],[1,2],[0,3],[3,4],[3,6],[3,7],[4,2],[4,5],[5,2]]
+n  = 7
+A = [[1,2],[2,5],[1,3],[1,4],[3,4],[2,6],[6,5]]
 
 M = []
-
-for i in range(n):
-    M.append([0]*n)
-
-# convert the input list into adjacency matrix
-for u,v in A:
-    M[u][v] = 1
-    M[v][u] = 1
-
-for row in M:
-    print(*row)
-
 # convert the input list into adjacency list
 
 D = defaultdict(list)
@@ -24,18 +12,37 @@ for u,v in A:
     # D[v].append(u) # for undirected
 
 # DFS
-
+# print(D)
 def dfs_recursive(node):
     print(node)
 
     for i in D[node]:
         if i not in seen:
             seen.add(i)
+
             dfs_recursive(i)
+def is_in(node)->bool:
+    if node in seen: return True
+    return False
 
 
-source = 0
+def insertion(val,neighbors):
+    if is_in(val):
+        print("The value is already in the graph")
+        return None
+
+    for i in neighbors:
+        if i in D.keys(): 
+            D[i].append(val)
+
+
+source = 1
 
 seen = set()
 seen.add(source)
-dfs_recursive(source)
+
+insertion(-1,[2,5])
+
+print(D)
+
+dfs_recursive(1)
